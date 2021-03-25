d590cf0c60e7c1736586c968a617507e5ca92856
Arshad Riyaz <arshadr@protonmail.com>
2021-03-25 17:31:27 +0800

    complete courseinfo exercise

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -6,38 +6,50 @@ const Header = (props) => (
 
 const Part = (props) => (
   <p>
-    {props.part} {props.exercise}
+    {props.name} {props.exercises}
   </p>
 )
 
 const Content = (props) => (
     <>
-        <Part part={props.part1} exercise={props.exercises1} />
-        <Part part={props.part2} exercise={props.exercises2} />
-        <Part part={props.part3} exercise={props.exercises3} />
+    {
+        props.parts.map(
+            part => <Part name={part.name} exercises={part.exercises}/>
+        )
+    }
     </>
 )
 
 const Total = (props) => (
-    <p>Number of exercises {props.total}</p>
+    <p>Number of exercises {props.parts.reduce((acc, cur) => acc + cur.exercises, 0)} </p>
 )
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+    const course = {
+        name: 'Half Stack application development',
+        parts:  [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+              name: 'Using props to pass data',
+              exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
-    </div>
-  )
+    return (
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
+        </div>
+    )
 }
 
 export default App

66905f4a8ffb0566e722fce31b62cbfcdff952e3
Arshad Riyaz <arshadr@protonmail.com>
2021-06-22 17:09:48 +0800

    Exercises 4.1-4.2

---
+++ part4/blog-list/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+logger.info('Connecting to MongoDB at ', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('Successfully connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('Error connecting to MongoDB ', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
